874c923e8c5e0f58152a41283090a8d5be481614
Anthony Defranceschi <chaacygg@gmail.com>
2017-11-27 01:14:15 +0100

    Add empty class-based views.

--- updater/urls.py
+++ updater/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'updater'
 urlpatterns = [
     django.conf.urls.url(r'^$', views.greetings, name='greetings'),
-    django.conf.urls.url(r'^file_list/$'),
-    # Get missing files with post request.
-    django.conf.urls.url(r'^fire_sending/$'),
+    # Returns the update file list.
+    django.conf.urls.url(r'^file_list/$', views.FileListView.as_view(), name='file_list'),
+    # Must receive a post request containing the missing files.
+    django.conf.urls.url(r'^fire_sending/$', views.MissingFileView.as_view(), name='fire_sending'),
 ]
